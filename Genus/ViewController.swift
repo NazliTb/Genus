81d178dd2d587dc0a4d47fc67635619868470281
Nazli Thabet <nazli.thabet@esprit.tn>
2020-11-18 09:46:52 +0100

    UI
    making of app UI interfaces

---
+++ Genus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Genus
+//
+//  Created by Nazli on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
