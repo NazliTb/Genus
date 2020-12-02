aa6700bd63a572d75dea8972f4dc681f93eff887
SihemMlaouah <44605840+SihemMlaouah@users.noreply.github.com>
2020-12-02 15:07:05 +0100

    Update ProfileActivity.java
    sorry forgot to push this

--- app/src/main/java/com/esprit/genus/ProfileActivity.java
+++ app/src/main/java/com/esprit/genus/ProfileActivity.java
@@ -12,10 +12,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.esprit.genus.Model.Game;
+import com.esprit.genus.Retrofit.INodeJS;
+import com.esprit.genus.Retrofit.RetrofitClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ProfileActivity extends AppCompatActivity {
     private TextView gameList, wishList, signOut,gamesNbr,favNbr,wishesNbr,username;
     private String idUser="";
-
+    INodeJS myAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +40,105 @@ public class ProfileActivity extends AppCompatActivity {
         gamesNbr = (TextView) findViewById(R.id.gamesNbr);
         favNbr = (TextView) findViewById(R.id.favNbr);
         wishesNbr = (TextView) findViewById(R.id.wishesNbr);
-        username= (TextView) findViewById(R.id.username);
+        username = (TextView) findViewById(R.id.username);
         //Recuperer les données de homepage
         Intent intent = getIntent();
-        if (intent != null){
+        if (intent != null) {
 
-            if (intent.hasExtra("idUser")){
+            if (intent.hasExtra("idUser")) {
                 idUser = intent.getStringExtra("idUser");
             }
-            if (intent.hasExtra("username")){
+            if (intent.hasExtra("username")) {
                 username.setText(intent.getStringExtra("username"));
             }
-            if (intent.hasExtra("gameNbr")){
-                gamesNbr.setText(intent.getStringExtra("gameNbr"));
-            }
-            if (intent.hasExtra("favNbr")){
-                favNbr.setText(intent.getStringExtra("favNbr"));
-            }
-            if (intent.hasExtra("wishNbr")){
-                wishesNbr.setText(intent.getStringExtra("wishNbr"));
-            }
+
 
         }
+        //Init API
+        Retrofit retrofit = RetrofitClient.getInstance();
+        //this one i used for the gamenumber , wishnumber , favnumber  because i'm using Gson okay ?
+        Retrofit retrofit1 = new Retrofit.Builder().baseUrl("http://10.0.2.2:3000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        myAPI = retrofit1.create((INodeJS.class));
+
+        Call<List<Game>> listWishGames = myAPI.GetWishList(Integer.parseInt(idUser));
+        Call<List<Game>> listFavGames = myAPI.GetFavList(Integer.parseInt(idUser));
+        Call<List<Game>> listGames = myAPI.GetGameListForNumber(Integer.parseInt(idUser));
+        listGames.enqueue(new Callback<List<Game>>() {
+            @Override
+            public void onResponse(Call<List<Game>> call, Response<List<Game>> response) {
+                if (!response.isSuccessful()) {
+
+                    return;
+                }
+                List<Game> Games = response.body();
+                int cpt=0;
+                for(Game g:Games)
+                {
+
+                    cpt++;
+                }
+                gamesNbr.setText(cpt+"");
+            }
+
+            @Override
+            public void onFailure(Call<List<Game>> call, Throwable t) {
+
+            }
+        });
+
+
+        listFavGames.enqueue(new Callback<List<Game>>() {
+            @Override
+            public void onResponse(Call<List<Game>> call, Response<List<Game>> response) {
+                if (!response.isSuccessful()) {
+
+                    return;
+                }
+                List<Game> FavGames = response.body();
+                int cpt=0;
+                for(Game g:FavGames)
+                {
+
+                    cpt++;
+                }
+                favNbr.setText(cpt+"");
+            }
+
+            @Override
+            public void onFailure(Call<List<Game>> call, Throwable t) {
+
+            }
+        });
+
+        listWishGames.enqueue(new Callback<List<Game>>() {
+            @Override
+            public void onResponse(Call<List<Game>> call, Response<List<Game>> response) {
+                if (!response.isSuccessful()) {
+
+                    return;
+                }
+                List<Game> wishGames = response.body();
+                int cpt=0;
+                for(Game g:wishGames)
+                {
+                 
+                    cpt++;
+                }
+                wishesNbr.setText(cpt+"");
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Game>> call, Throwable t) {
+
+            }
+        });
+
 
         gameList.setOnClickListener(new View.OnClickListener() {
             @Override
