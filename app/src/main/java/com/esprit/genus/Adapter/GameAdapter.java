3d074cc14633bccd6e2a8b5535326e10cb68053d
Nazli Thabet <nazli.thabet@esprit.tn>
2020-12-05 01:20:24 +0100

    Added app icon and display images from DB (still not working)

--- app/src/main/java/com/esprit/genus/Adapter/GameAdapter.java
+++ app/src/main/java/com/esprit/genus/Adapter/GameAdapter.java
@@ -1,8 +1,11 @@
 package com.esprit.genus.Adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,7 +14,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.esprit.genus.Model.Game;
 import com.esprit.genus.R;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.util.List;
 
 public class GameAdapter extends RecyclerView.Adapter<GameAdapter.MyViewHolder> {
@@ -31,6 +36,19 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.MyViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        File myImg = new File(gameList.get(position).getGamePicture());
+        //Picasso.get().load(myImg).into(holder.gamePic);
+
+        /*InputStream is = null;
+        try {
+            is = new URL(gameList.get(position).getGamePicture()).openStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = BitmapFactory.decodeStream(is);*/
+        Bitmap myBitmap = BitmapFactory.decodeFile(myImg.getAbsolutePath());
+
+        holder.gamePic.setImageBitmap(myBitmap);
         holder.gameName.setText(gameList.get(position).getName());
         holder.gameStudio.setText(gameList.get(position).getCompanyName());
         holder.gameType.setText(gameList.get(position).getType());
@@ -42,19 +60,21 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.MyViewHolder> 
         return gameList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
 
         CardView root_view;
         TextView gameName, gameStudio, gameType, gameDate;
+        ImageView gamePic;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            root_view = (CardView)itemView.findViewById(R.id.root_view);
-            gameName = (TextView)itemView.findViewById(R.id.txt_name);
-            gameStudio = (TextView)itemView.findViewById(R.id.txt_companyname);
-            gameType = (TextView)itemView.findViewById(R.id.txt_type);
-            gameDate = (TextView)itemView.findViewById(R.id.txt_date);
+            root_view = (CardView) itemView.findViewById(R.id.root_view);
+            gamePic = (ImageView) itemView.findViewById(R.id.gamePic);
+            gameName = (TextView) itemView.findViewById(R.id.txt_name);
+            gameStudio = (TextView) itemView.findViewById(R.id.txt_companyname);
+            gameType = (TextView) itemView.findViewById(R.id.txt_type);
+            gameDate = (TextView) itemView.findViewById(R.id.txt_date);
 
         }
     }
