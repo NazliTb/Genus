5653b7a9059174340e20f709c37178381f098f17
SihemMlaouah <44605840+SihemMlaouah@users.noreply.github.com>
2020-12-08 19:40:35 +0100

    updates design tab bar

---
+++ Genus/ChatController.swift
@@ -0,0 +1,27 @@
+//
+//  ChatController.swift
+//  Genus
+//
+//  Created by Orionsyrus24 on 12/8/20.
+//
+
+import UIKit
+
+class ChatController: UIViewController {
+    
+    //Widgets
+    
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    //IBActions
+    
+   
+    @IBAction func searchAction(_ sender: Any) {
+    }
+}
---
+++ Genus/DiscoverController.swift
@@ -0,0 +1,27 @@
+//
+//  DiscoverController.swift
+//  Genus
+//
+//  Created by Orionsyrus24 on 12/8/20.
+//
+
+import UIKit
+
+class DiscoverController: UIViewController {
+    
+    //Widgets
+    
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    //IBActions
+    
+   
+    @IBAction func searchAction(_ sender: Any) {
+    }
+}
