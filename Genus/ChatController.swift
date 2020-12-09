73249e883fd8c152b555d15dcb60242159b65c39
SihemMlaouah <44605840+SihemMlaouah@users.noreply.github.com>
2020-12-09 21:29:51 +0100

    updates
    added  get topics web service

--- Genus/ChatController.swift
+++ Genus/ChatController.swift
@@ -18,6 +18,29 @@ class ChatController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //Functions
+    func GetChatList() {
+    let url=URL(string: "http://192.168.64.1:3000/GetChatList")!
+    // let url = URL(string: "http://192.168.247.1:3000/GetChatList")!)
+    let task = URLSession.shared.dataTask(with: url, completionHandler:{ data, response, error in guard let data = data else { return }
+    do {
+        let json = try JSONSerialization.jsonObject(with: data) as! Dictionary<String, AnyObject>
+    let idChat = json["idChat"] as! Int
+    let topic=json["topic"] as! String
+    let Date=json["Date"] as! String
+    let user=json["username"] as! String
+    
+   
+        
+   
+    }
+    catch let parseErr {
+    print(parseErr)
+    
+    }
+    })
+    task.resume()
+    }
 
     //IBActions
     
